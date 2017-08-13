e913d3c177b0b5321fe391533ee05d37dcbfb462
sankalp139 <sankalp139@gmail.com>
2017-08-13 23:06:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="New Value"
 
+var image=document.getElementById("madi");
+image.onclick =  function() {
+    image.style.marginLeft ='180px';
+    
+};
