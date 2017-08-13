2eff65942b6b8ca05d5abf24e0df13ad85dab6b2
sankalp139 <sankalp139@gmail.com>
2017-08-13 23:14:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,12 @@ var element=document.getElementById("main-text");
 element.innerHTML="New Value"
 
 var image=document.getElementById("madi");
+var marginLeft=0;
+function moveRight (){
+    marginLeft=marginLeft+10;
+    image.style.marginLeft=marginLeft+ 'px';
+}
 image.onclick =  function() {
-    image.style.marginLeft ='180px';
+    var interval=setInterval(moveRight,50);
     
 };
