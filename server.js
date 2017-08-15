6019a797d8f0bc7e3a088a18f0884fb4311ccb56
sankalp139 <sankalp139@gmail.com>
2017-08-15 23:48:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,9 +43,7 @@ var artices ={
     
 };
 
-app.get('/',function(req,res){
- res.sendFile(path.join(_dirname,'ui','index.html'))   
-})
+
 function createTemplate(data){
     var title=data.title;
     var heading=data.heading;
@@ -78,21 +76,24 @@ var htmlTemplate =
     </html>`;
     return htmlTemplate;
     }
-
+app.get('/:articleName',function(req,res){
+ res.send(createTemplate(articles[articleName]));
+});
 var counter=0;
 app.get('/counter',function(req,res){
   counter=counter+1;
   res.send(counter.toString());
 });
-app.get('/:articleName',function(req,res){
- res.send(createTemplate(articles[articleName]));
+app.get('/',function(req,res){
+ res.sendFile(path.join(_dirname,'ui','index.html'))   
 });
+
 app.get('/article-two',function(req,res){
  res.sendFile(path.join(_dirname,'ui','article-two.html'))   
-})
+});
 app.get('/article-three',function(req,res){
  res.sendFile(path.join(_dirname,'ui','article-three.html'))   
-})
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
