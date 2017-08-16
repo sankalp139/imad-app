6db9c785423c134db366c26b9d84d615888222cf
sankalp139 <sankalp139@gmail.com>
2017-08-16 22:40:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/counter',function(req,res){
   counter=counter+1;
   res.send(counter.toString());
 });
-app.get('/ui/',function(req,res){
+app.get('/',function(req,res){
    res.sendFile(path.join(_dirname,'ui','index.html'));   
   });
 
