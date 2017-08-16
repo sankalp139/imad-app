04b4358dcc1ca282aac8e6626ebad5f224b0bcec
sankalp139 <sankalp139@gmail.com>
2017-08-16 22:38:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,10 +88,10 @@ app.get('/',function(req,res){
  });
 
 app.get('/article-two',function(req,res){
- res.sendFile(path.join(_dirname,'ui','article-two.html'))   
+ res.sendFile(path.join(_dirname,'ui','article-two.html'));   
 });
 app.get('/article-three',function(req,res){
- res.sendFile(path.join(_dirname,'ui','article-three.html'))   
+ res.sendFile(path.join(_dirname,'ui','article-three.html'));   
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
