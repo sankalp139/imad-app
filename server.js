102843f04788200a23011497fda478b20ec16111
sankalp139 <sankalp139@gmail.com>
2017-08-18 19:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ var htmlTemplate =
     return htmlTemplate;
     }
 
-app.get('/',function(req,res){
+app.get('/ui/',function(req,res){
    res.sendFile(path.join(_dirname,'ui','index.html'));   
   });
 
