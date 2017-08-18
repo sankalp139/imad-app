6357f94626598948606bd331b18a1aa7b17a7c0b
sankalp139 <sankalp139@gmail.com>
2017-08-18 23:29:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,7 @@
-console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHTML="New Value"
-
-var image=document.getElementById("madi");
-var marginLeft=0;
-function moveRight (){
-    marginLeft=marginLeft+1;
-    image.style.marginLeft=marginLeft+ 'px';
+var button =document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter = counter + 1;
+    var span =document.getElementById('count');
+    span .innerHTML =counter.toString();
 }
-image.onclick =  function() {
-    var interval=setInterval(moveRight,50);
-    
-};
