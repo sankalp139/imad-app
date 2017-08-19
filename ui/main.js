4d2e72b207e789a76b75fbf13686d7cd7035a5dc
sankalp139 <sankalp139@gmail.com>
2017-08-20 00:31:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,13 +17,36 @@ button.onclick = function(){
     
   request.send(null);
 };
+// button.onclick = function(){
+    
+//     var request= new XMLHttpRequest();
+//     request.onreadystatechange = function (){
+//         if(request.readyState == XMLHttpRequest.DONE){
+//             if(request.status == 200){
+//         var counter = request.responseText;
+//       var span =document.getElementById('count');
+//         span .innerHTML =counter.toString();
+//             }
+//         }
+//     };
+    request.open('GET','http://sankalp139.imad.hasura-app.io/counter',true);
+    //http://sankalp139.imad.hasura-app.io/counter
+    
+  request.send(null);
+};
 var nameInput =document.getElementById('name');
 var name = nameInput.value;
 var submit= document.getElementById('submit_btn'); 
 submit.onclick = function()
 {
     //make a request to the server  and accept name
-    var names=['name1','name2','name3','name4'];
+     var request= new XMLHttpRequest();
+    request.onreadystatechange = function (){
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status == 200){
+       
+    var names=request.responseText;
+    names=JSON.parse(names);
     var list="";
     for(var i=0; i<names.length ;i++)
     {
@@ -31,4 +54,11 @@ submit.onclick = function()
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML =list;
+            }
+        }
+    };
+    request.open('GET','http://sankalp139.imad.hasura-app.io/submit-name?name=',name,true);
+    //http://sankalp139.imad.hasura-app.io/counter
+    
+  request.send(null);
 }
