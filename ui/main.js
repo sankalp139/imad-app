62070bcedaee84bfc73b656d4451cc5393ca98d5
sankalp139 <sankalp139@gmail.com>
2017-08-19 19:59:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,18 @@ button.onclick = function(){
     
   request.send(null);
 };
+var nameInput =document.getElementById('name');
+var name = nameInput.value;
+var submit= document.getElementById('submit_btn'); 
+submit.onclick = function()
+{
+    //make a request to the server  and accept name
+    var names=['name1','name2','name3','name4'];
+    var list="";
+    for(var i=0; i<names.length ;i++)
+    {
+        list+= '<li>'+ name[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML =list;
+}
