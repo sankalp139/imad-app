b6bc631aa0d4473d85504bc3c2813d197f727fa6
sankalp139 <sankalp139@gmail.com>
2017-09-03 16:22:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ var htmlTemplate =
     function hash(input,salt){
         // to hash the data taken
         var hash=crypto.pbkdf2Sync(input, salt,10000,512,'sha512');
-        return hash.toString('hex');
+        return ['pbkdf2Sync','10000',salt,hash.toString('hex')].join('$');
         
     }
      app.get('/hash/:input',function(req,res){
