b69c386e97169b67a1c5a551d96d3c7be2070b2a
sankalp139 <sankalp139@gmail.com>
2017-09-04 21:40:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var articles = {
         </p>`}
     
 };
-//function createTemplate(data){
+function createTemplate(data){
     var title=data.title;
     var heading=data.heading;
     var date=data.date;
