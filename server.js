fedf65865957bce96dacf89b6b0c954658051573
sankalp139 <sankalp139@gmail.com>
2017-09-04 19:40:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,33 +87,33 @@ function createTemplate(data){
     var heading=data.heading;
     var date=data.date;
      var content=data.content;
-var htmlTemplate =
-`<html>
-    <head>
-    <title>
-            ${title}   
-    </title>
-     <meta name="viewport" content="width-device-width,initial-scale-1"/>
-      <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-          <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-             ${heading}   
-        </h3>
-        <div>
-         ${date}   
-        </div>
-        ${content}
-        </div>
-    </body>
-    </html>`;
-    return htmlTemplate;
-    }
+// var htmlTemplate =
+// `<html>
+//     <head>
+//     <title>
+//             ${title}   
+//     </title>
+//      <meta name="viewport" content="width-device-width,initial-scale-1"/>
+//       <link href="/ui/style.css" rel="stylesheet" />
+//     </head>
+//     <body>
+//         <div class="container">
+//         <div>
+//           <a href="/">Home</a>
+//         </div>
+//         <hr/>
+//         <h3>
+//              ${heading}   
+//         </h3>
+//         <div>
+//          ${date}   
+//         </div>
+//         ${content}
+//         </div>
+//     </body>
+//     </html>`;
+//     return htmlTemplate;
+//     }
 var counter = 0;
 app.get('/counter',function(req,res){
   counter = counter + 1;
