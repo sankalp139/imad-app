902504ceb1bc27c1044888578f2fc884b0c4fc98
sankalp139 <sankalp139@gmail.com>
2017-09-05 19:55:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,4 @@
-var button =document.getElementById('counter');
-
-button.onclick = function(){
-    
-    var request= new XMLHttpRequest();
-    request.onreadystatechange = function (){
-        if(request.readyState == XMLHttpRequest.DONE){
-            if(request.status == 200){
-        var counter = request.responseText;
-       var span =document.getElementById('count');
-        span .innerHTML =counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://sankalp139.imad.hasura-app.io/counter',true);
-    //http://sankalp139.imad.hasura-app.io/counter
-    
-  request.send(null);
-};
-
+//submit username and password
 
 var submit= document.getElementById('submit_btn'); 
 submit.onclick = function()
@@ -27,22 +8,25 @@ submit.onclick = function()
     request.onreadystatechange = function (){
         if(request.readyState == XMLHttpRequest.DONE){
             if(request.status == 200){
-       var names=request.responseText;
-      names=JSON.parse(names);
-    var list="";
-    for(var i=0; i<names.length ;i++)
-    {
-        list += '<li>'+ names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML =list;
+       console.log('user has logged in');
+       alert('logged in successfully');
+            }else if(request.status == 403)
+            {
+                alert('username/password is incorrect');
+            }
+            else if(request.status==500)
+            
+            {
+            alert('something went wrong on the server');    
             }
         }
     };
-    var nameInput =document.getElementById('name');
- var name = nameInput.value;
-   request.open('GET','http://sankalp139.imad.hasura-app.io/submit-name?name='+ name,true);
+    var username =document.getElementById('username').value;
+     var password =document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+   request.open('POST','http://sankalp139.imad.hasura-app.io/login'+ name,true);
     //http://sankalp139.imad.hasura-app.io/counter
-    
-  request.send(null);
+    request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username:username,password:password}));
 };
