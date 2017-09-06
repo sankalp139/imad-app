9d85f4a95e54033bf0fd8c334187847488b09ed3
sankalp139 <sankalp139@gmail.com>
2017-09-06 13:15:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config ={
     database:'sankalp139',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.emv.DB_PASSWORD};
+    password:process.env.DB_PASSWORD};
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
