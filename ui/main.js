56b4cf09a2b9f859640e1f41342086b68b522ce3
sankalp139 <sankalp139@gmail.com>
2017-09-27 20:31:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit.onclick = function()
      var password =document.getElementById('password').value;
     console.log(username);
     console.log(password);
-   request.open('POST','/login'+ name,true);
+   request.open('POST','/login',true);
     //http://sankalp139.imad.hasura-app.io/counter
     request.setRequestHeader('Content-Type','application/json');
   request.send(JSON.stringify({username:username,password:password}));
