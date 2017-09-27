6bd2fd8dd62273e8a2b322bc84387ae50eb85d74
sankalp139 <sankalp139@gmail.com>
2017-09-27 15:13:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -165,10 +165,23 @@ app.get('/submit-name', function(req,res){
       res.send(JSON.stringify(names));
   });
   app.get('/articles/:articleName',function(req,res){
-   var articleName=req.params.articleName;
-   pool.query("SELECT * FROM article")
+   pool.query("SELECT * FROM article WHERE title = " + req.params.articleName,function(err,result){
+        if(err){
+         res.status(500).send(err.toString());
+        }else 
+        {
+            if(result.rows.length ===0){
+                res.status(404).send('Article Not Found');
+            }
+            else
+            {
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+   });
    
-  res.send(createTemplate(articleData));
+  
  });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
