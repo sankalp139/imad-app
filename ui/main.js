33634711d9044d30f8a5e15c16ed6acdb424acab
sankalp139 <sankalp139@gmail.com>
2017-09-28 20:07:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,9 @@ function loadLoginForm () {
         <br/><br/>
         <input type="submit" id="login_btn" value="Login" />
         <input type="submit" id="register_btn" value="Register" />
-        `}
+        `;
     document.getElementById('login_area').innerHTML = loginHtml;
-var submit= document.getElementById('submit_btn'); 
+var submit= document.getElementById('login_btn'); 
 submit.onclick = function()
 {
     //make a request to the server  and accept name
@@ -17,17 +17,22 @@ submit.onclick = function()
     request.onreadystatechange = function (){
         if(request.readyState == XMLHttpRequest.DONE){
             if(request.status == 200){
-       console.log('user has logged in');
+     //  console.log('user has logged in');
+           submit.value = 'Sucess!';
        alert('logged in successfully');
             }else if(request.status == 403)
             {
-                alert('username/password is incorrect');
+                //alert('username/password is incorrect');
+                submit.value = 'Invalid credentials. Try again?';
             }
             else if(request.status==500)
             
             {
-            alert('something went wrong on the server');    
+            alert('something went wrong on the server'); 
+           submit.value = 'Login';
             }
+        
+            loadLogin();
         }
     };
      var username =document.getElementById('username').value;
@@ -38,6 +43,7 @@ submit.onclick = function()
     //http://sankalp139.imad.hasura-app.io/counter
     request.setRequestHeader('Content-Type','application/json');
   request.send(JSON.stringify({username:username,password:password}));
+    submit.value = 'Logging in...';
 };
     var register=document.getElementById('register_btn');
     register.onclick =function()
@@ -50,9 +56,11 @@ submit.onclick = function()
               // Take some action
               if (request.status === 200) {
                   alert('User created successfully');
+                  register.value = 'Registered!';
                   } 
                   else {
                   alert('Could not register the user');
+                  register.value = 'Register';
                        }
                }
         };
@@ -67,4 +75,6 @@ submit.onclick = function()
     //http://sankalp139.imad.hasura-app.io/counter
     request.setRequestHeader('Content-Type','application/json');
   request.send(JSON.stringify({username:username,password:password}));
+   register.value = 'Registering...';
 };
+}
