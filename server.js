dcce98396730e7ead4545913d8a60876fa3a0d44
sankalp139 <sankalp139@gmail.com>
2017-10-07 00:53:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/',function(req,res){
             else
             {
                 //var articleData=result.rows[0];
-                res.send(articleData);
+                res.send(result.rows[0]);
             }
         }
         });  
