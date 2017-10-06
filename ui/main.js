18f57c3f466dd6a7e72cab1484ac7084593d0c09
sankalp139 <sankalp139@gmail.com>
2017-10-06 11:58:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ submit.onclick = function()
     //make a request to the server  and accept name
      var request= new XMLHttpRequest();
     request.onreadystatechange = function (){
-        if(request.readyState == 4){
+        if(request.readyState === 4){
             if(request.status == 200){
       console.log('user has logged in');
            submit.value = 'Sucess!';
