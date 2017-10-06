aef763c68f7c6e6229ac7fc698b71e5a4efbd427
sankalp139 <sankalp139@gmail.com>
2017-10-07 00:48:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ submit.onclick = function()
             if(request.status === 200){
       console.log('user has logged in');
            submit.value = 'Sucess!';
-           window.location.href ="http://sankalp139.imad.hasura-app.io/articles/article-one"
+           window.location.href ="http://sankalp139.imad.hasura-app.io/blog";
        alert('logged in successfully');
             }else if(request.status === 403)
             {
